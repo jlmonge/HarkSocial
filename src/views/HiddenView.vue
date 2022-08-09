6a90c1c652dc105d19848056b8d33eae5f3de5b6
Joseph Monge <josephmonge00@gmail.com>
2022-08-09 22:04:10 +0200

    Complete homepage appearance

---
+++ src/views/HiddenView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hidden">
+    <h1>This is a hidden page you cannot see this</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .hidden {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
