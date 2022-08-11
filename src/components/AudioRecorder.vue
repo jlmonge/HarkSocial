091b2b2c715b7519cd52e410f5c3907a96782da7
Kenneth <kgela001@ucr.edu>
2022-08-12 00:05:46 +0200

    progress on recorder

---
+++ src/components/AudioRecorder.vue
@@ -0,0 +1,16 @@
+<template>
+  <tapir-widget :time="2" backendEndpoint="https://your-endpoint.com/.netlify/functions/audio-message" 
+                buttonColor="green"/>
+</template>
+
+<script>
+import TapirWidget from 'vue-audio-tapir';
+import 'vue-audio-tapir/dist/vue-audio-tapir.css';
+
+export default {
+  name: 'App',
+  components: {
+    TapirWidget,
+  }
+}
+</script>
---
+++ src/views/Recorder.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import TapirWidget '../components/AudioRecorder.vue'
+</script>
+
+<template>
+    <div class="conversation">
+        <Prompt></Prompt>
+        <ConversationFeed></ConversationFeed>
+    </div>
+</template>
