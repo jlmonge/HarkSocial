d1b6353de4dcf47ec7b043f0d658d2abfd1f655d
Kenneth <kgela001@ucr.edu>
2022-08-16 00:55:10 +0200

    Logging in get all user data from the database in the console

---
+++ src/components/ReadDatabase.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import { getFirestore,collection, getDocs, snapshotEqual } from '@firebase/firestore';
+    import { getAuth , onAuthStateChanged} from '@firebase/auth'
+
+    const db = getFirestore();
+    const colRef = collection(db,'users')
+
+    export function getUser() {
+        getDocs(colRef)
+        .then(snapshot) => {
+            let allUsers =  []
+            snapshotEqual.docs.forEach((doc)=>{
+                allUsers.push({ ...doc.data() , id: doc.id})
+            })
+            console.log(allUsers)
+        }
+        .catch(err => {
+            console.log(err.message)
+        })
+    }
+    
+</script>
+
+<template>
+  
+</template>
+
+
+<style scoped>
+
+</style>
--- src/views/Conversation.vue
+++ src/views/Conversation.vue
@@ -2,6 +2,24 @@
     import ConversationFeed from '@/components/ConversationFeed.vue'
     import Prompt from '@/components/Prompt.vue'
     import NavBar from '../components/NavBar.vue';
+    import { getFirestore,collection, getDocs, snapshotEqual } from '@firebase/firestore';
+    const db = getFirestore();
+    const colRef = collection(db,'users')
+    
+        getDocs(colRef)
+        .then((snapshot) => {
+            let allUsers = [];
+            snapshot.docs.forEach((doc)=>{
+                allUsers.push({ ...doc.data() , id: doc.id})
+            })
+            console.log(allUsers)
+        })
+        .catch(err => {
+            console.log(err.message)
+        })
+
+        
+        
 </script>
 
 <template>
