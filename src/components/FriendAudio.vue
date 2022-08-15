60bc87a0e763a8f28f3259dd8dfcafd0ba3e0525
jessejamesss <jessejames.ro@gmail.com>
2022-08-15 20:12:00 +0200

    Committing FriendAudio

---
+++ src/components/FriendAudio.vue
@@ -0,0 +1,39 @@
+<script setup>
+    //import AvBar from '../../node_modules/vue-audio-visual/src/components/AvBars'
+    import { getDownloadURL, getStorage, ref } from "firebase/storage";
+    //import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+    import firebase from 'firebase/compat';
+    // Create a reference with an initial file path and name
+    //function getStorageAudio() {
+        const storage = getStorage();
+        const storageRef = ref(storage);
+
+        // const today = new Date();
+        // const date = `${today.getFullYear()}` + `${(today.getMonth() + 1)}` + `${today.getDate()}`
+        // const user = firebase.auth().currentUser.uid
+
+        const friendPath = 'VhDlHVyyx9f4bXlZdyNSyuxTG6k12022815'
+
+        const friendAudioRef = ref(storageRef, `${ friendPath }`)
+
+        getDownloadURL(friendAudioRef)
+            .then((url) => {
+                const audioControls = document.getElementById('vis2')
+                audioControls.setAttribute('src', url)
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+
+</script>
+
+<template>
+    <audio id="vis2" src="" controls>
+        <source type="audio/mpeg">
+    </audio>
+</template>
+
+
+<style scoped>
+
+</style>
