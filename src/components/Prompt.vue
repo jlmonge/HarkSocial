4fa6d1ba2591ace54fb9248012f921d4a12599b9
Christopher De Santiago <chris@Christophers-MacBook-Pro.local>
2022-08-23 22:26:28 +0200

    pushing to firebase, prompt for admin works

--- src/components/Prompt.vue
+++ src/components/Prompt.vue
@@ -1,14 +1,77 @@
-<script setup>
-
-</script>
-
-<template>
-    <h3>Prompt: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
-</template>
-
-<style scoped>
-h3 {
-    margin: 50px;
-    text-align: center;
+<script>
+import { db } from '@/firebase'
+import {doc,getDoc,setDoc,} from 'firebase/firestore'
+ 
+export default {
+ data() {
+   return {
+     prompts: [],
+     prompt: null,
+     readPrompts: null,
+     promptID: null,
+     };
+ },
+ methods: {
+   async createPrompt() {
+     if (this.prompt != null) {
+       try {
+ 
+         const date = new Date();
+         let day = date.getDate();
+         let month = date.getMonth() + 1;
+         let year = date.getFullYear();
+         let currentDate = `${month}-${day}-${year}`;
+ 
+         const docReference = await setDoc(
+           doc(db, 'prompts',currentDate),
+           {
+             prompt: this.prompt,
+           }
+         );
+       } catch(err) {
+         console.error(err);
+       }
+     }
+   },
+ 
+   async readPrompt() {
+     if (this.promptID != null) {
+       try {
+         const docReference = doc(db, 'prompts', this.promptID);
+ 
+         const response = await getDoc(docReference);
+ 
+         this.readPrompts = {
+           id: response.id, ...response.data(),
+         }
+       } catch(err) {
+         console.error(err);
+       }
+     }
+   }
+ }
 }
-</style>
+</script>
+ 
+<template>
+ <div>
+  
+   <input type="text" v-model="prompt"/>
+   <br>
+   <button @click="createPrompt()">Create Prompt</button>
+   <br>
+ 
+   <h3>Enter Today's Date for Today's Prompt</h3>
+ 
+   <input type="text" v-model="promptID" placeholder="Eg: 1/23/2022" />
+ 
+   <br>
+ 
+   <button @click="readPrompt()">Look At Prompt</button>
+ 
+   <template v-if="readPrompts">
+     <p>{{ readPrompts.prompt || 'Check the Date' }}</p>
+   </template>
+ 
+ </div>
+</template>
