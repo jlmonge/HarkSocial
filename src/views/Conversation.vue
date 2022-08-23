94d7aae020b71a1ae04a87e11b306e1413bff597
jessejamesss <jessejames.ro@gmail.com>
2022-08-23 19:08:57 +0200

    Committing incomplete shufflePairs function

--- src/views/Conversation.vue
+++ src/views/Conversation.vue
@@ -4,12 +4,29 @@ import Prompt from '@/components/Prompt.vue'
 import NavBar from '../components/NavBar.vue';
 import firebase from 'firebase/compat';
 import { userStore } from '../stores/UserStore.js'
+//import { forEach } from 'cypress/types/lodash';
 const main = userStore();
 
-import { getFirestore, collection, getDocs, snapshotEqual } from '@firebase/firestore'
+import { getFirestore, collection, getDocs, query, where, snapshotEqual } from '@firebase/firestore'
+// import {
+//   collection,
+//   doc,
+//   addDoc,
+//   getDoc,
+//   getDocs,
+//   query,
+//   where,
+//   deleteDoc,
+// } from 'firebase/firestore'
 const db = getFirestore()
 const userRef = collection(db, 'users')
+const friendsRef = collection(db, 'friends')
+const pairsRef = collection(db, 'pairs')
+
 const allUsers = []
+const allFriends = []
+const allPairs = []
+var friendsList = []
 
 getDocs(userRef)
     .then((snapshot) => {
@@ -21,6 +38,86 @@ getDocs(userRef)
     .catch(err => {
         console.log(err.message)
     })
+
+getDocs(friendsRef)
+    .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+            allFriends.push({...doc.data(), id: doc.id})
+        })
+        console.log(allUsers)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+
+getDocs(pairsRef)
+    .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+            allPairs.push({...doc.data(), id: doc.id})
+        })
+        console.log(allUsers)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+
+function shufflePairs() { 
+    allUsers.forEach(user => {
+        // Reset currentPair and friendsList for each user iteration
+        let currentPair = ''
+        friendsList = []
+        let paired = false
+        let nonexisting = true
+
+        // Find the friends of each user from the friends collection
+        for (let i = 0; i < allFriends.length; i++) {
+            if (user.email === allFriends[i].user1) {
+                friendsList.push(allFriends[i].user2)
+            }
+            if (user.email === allFriends[i].user2) {
+                friendsList.push(allFriends[i].user1)
+            }
+        }
+        //Find the user's current pair and set pairedBefore to true
+        for (let i = 0; i < allPairs.length; i++) {
+            if (user.email === allPairs[i].pair1 && allPairs[i].isPair) {
+                currentPair = allPairs[i].pair2
+                allPairs[i].pairedBefore = true
+            }
+            if (user.email === allPairs[i].pair2 && allPairs[i].isPair) {
+                currentPair = allPairs[i].pair1
+                allPairs[i].pairedBefore = true
+            }
+        }
+        console.log(user.email + '\'s current pair is: ' + currentPair)
+
+        // Pair these fuckers
+        while( !paired ) {
+            // Assign a random friend to be matched with the user
+            let random = Math.floor(Math.random() * user.friendsList.length)
+            let potentialPair = friendsList[random]
+
+            // Check if there is a non-existing potentialPair and user matching
+            // If so, addDoc that pairing
+            for(let i = 0; i < allPairs.length; i++) {
+                if (user.email === allPairs[i].pair1 && allPairs[i].pair2 === potentialPair) {
+                    nonexisting = false
+                    continue
+                }
+                if (user.email === allPairs[i].pair2 && allPairs[i].pair1 === potentialPair) {
+                    nonexisting = false
+                    continue
+                }
+            }
+            // if (nonexisting) {
+            //      addDoc when finished
+            // }
+            console.log(user.email + '\'s new pair is: ' + potentialPair)
+            paired = true
+        }
+    })
+}
+
 
 function shuffle() {
     allUsers.forEach(user => {
@@ -42,33 +139,30 @@ function shuffle() {
             }
 
             let friend = user.friendsList[random]
-            while(user)
+            
             if (user.hasPair === false) {
                 user.userPairs.push(friend)
                 user.hasPair = true
-
-                for (let i = 0; i < allUsers.length; i++) {
+            }
+            for (let i = 0; i < allUsers.length; i++) {
                     if (allUsers[i].email === friend) {
                         allUsers[i].userPairs.push(user.email)
                         allUsers[i].hasPair = true
                         break
                     }
-                }
             }
             break
         }
     })
     console.log(allUsers)
 }
-
-
 </script>
 
 
 <template>
     <NavBar></NavBar>
     <div class="conversation">
-        <button @click="shuffle()">Shuffle Pairs</button>
+        <button @click="shufflePairs()">Shuffle Pairs</button>
         <button @click="main.printMessage()">Print</button>
         <Prompt></Prompt>
         <ConversationFeed></ConversationFeed>
